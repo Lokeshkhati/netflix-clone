64321e480ce56facaca7303abf75f029c6277446
Lokesh Khati <lokeshkhati005@gmail.com>
2022-03-08 13:28:52 +0530

    completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.addEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1198px-Netflix_2015_logo.svg.png?20190206123158"
+      />
+
+      <button className=" banner__button  SignIn__button">Sign In</button>
+    </div>
+  );
+};
+
+export default Navbar;
